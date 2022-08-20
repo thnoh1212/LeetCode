d4e3ac77dc973aa1eab6e39ab574cce1e2365da6
thnoh1212 <40189618+thnoh1212@users.noreply.github.com>
2022-08-21 01:38:14 +0900

    Time: 25 ms (73.59%), Space: 54.1 MB (47.08%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.java
+++ 875-koko-eating-bananas/875-koko-eating-bananas.java
@@ -9,14 +9,12 @@ class Solution {
         int mid = (min + max) / 2;
         while(min != max){
             mid = (max + min) / 2;
-            System.out.print(min + " " + max + " " + mid + " ");
             int day = 0;
             for(int i = 0; i < piles.length; i++){
                 day += piles[i] / mid;
                 if(piles[i] % mid != 0)
                     day += 1;
             }
-            System.out.println(day);
             if(day > h){
                 min = mid;
             }else if(day <= h){
