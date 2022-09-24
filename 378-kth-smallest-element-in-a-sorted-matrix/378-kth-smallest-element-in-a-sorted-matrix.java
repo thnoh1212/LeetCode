0121fc7cd2a9c8e508b738e13f1f71dcda613371
thnoh1212 <40189618+thnoh1212@users.noreply.github.com>
2022-09-25 02:14:37 +0900

    Time: 101 ms (5.16%), Space: 56.3 MB (73.50%) - LeetHub

---
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        
+        Comparator<Integer> comparator = (a, b) -> a.compareTo(b);
+        Map<Integer, Integer> map = new TreeMap<>();
+        int res = 0;
+        
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[i].length; j++){
+                if(map.containsKey(matrix[i][j])){
+                    int val = map.get(matrix[i][j]);
+                    map.put(matrix[i][j], val + 1);
+                }
+                else{
+                    map.put(matrix[i][j], 1);
+                }
+            }
+        }
+        
+        for(int key : map.keySet()){
+            k -= map.get(key);
+            if(k <= 0){
+                res = key;
+                break;
+            }
+        }
+        
+        return res;
+    }
+}
